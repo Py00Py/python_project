8763b9e475790b57b519439597b8da78ed25ec03
Shalok <96857472+Py00Py@users.noreply.github.com>
2023-12-15 19:51:07 +0530

    Update tele b system.py

--- tele b system.py
+++ tele b system.py
@@ -1,5 +1,5 @@
 print("""***************TELEphONE billING SYSTEM ************************** \n*******Designed and Maintained By :
-\n*******Shalok********
+\n*******Shalok********""")
 print()
 print()
 import mysql.connector as mq
